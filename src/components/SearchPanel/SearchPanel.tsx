561774939e3c9fec992f17b79ad6e1511610b7ef
avarlamova <59831804+avarlamova@users.noreply.github.com>
2023-01-03 15:48:54 +0300

    add react hook form

--- src/components/SearchPanel/SearchPanel.tsx
+++ src/components/SearchPanel/SearchPanel.tsx
@@ -1,29 +1,42 @@
-import React from 'react';
-import { useForm, SubmitHandler } from "react-hook-form";
-import styles from './SearchPanel.module.css'
-import {ReactComponent as SearchIcon} from './search.svg'
+import React from "react";
+import { useForm } from "react-hook-form";
+import styles from "./SearchPanel.module.css";
+import { ReactComponent as SearchIcon } from "./search.svg";
 
 type InputType = {
-    query: string,
+  query: string;
 };
 
 const SearchPanel = () => {
-    const { register, handleSubmit } = useForm<InputType>();
-    const onSubmit: SubmitHandler<InputType> = (formInput) =>
-    {
-        console.log(formInput);
-    }
+  const { register, handleSubmit, setValue } = useForm<InputType>();
 
-    return (
-        <>
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input className={styles.searchBar} {...register("query")} placeholder={"enter book name"} />
-            {/* <input type="submit" /> */}
-        <button type="submit"><SearchIcon /></button>
-        </form>
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement; //https://bobbyhadz.com/blog/typescript-property-value-not-exist-type-eventtarget
+    //   Спустя секунду после ввода последнего символа текста в поисковую строку автоматически должен начаться поиск книг. Если в течении секунды пользователь вводит что-то дополнительно, таймер должен сбрасываться.
 
-        </>
+    setValue("query", target.value);
+    setTimeout(
+      handleSubmit((e) => console.log(e)),
+      1000
     );
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit((data) => console.log(data))}>
+        <input
+          className={styles.searchBar}
+          {
+            ...(register("query"), { onChange: handleChange }) // instead of onchange handler
+          }
+          placeholder={"enter book name"}
+        />
+        <button type="submit">
+          <SearchIcon />
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default SearchPanel;
