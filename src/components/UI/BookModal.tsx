f26ff8fe683d63aa6a069e0a0db2d7cc893312fb
avarlamova <59831804+avarlamova@users.noreply.github.com>
2023-01-06 17:41:57 +0300

    add modal wrapper

--- src/components/UI/BookModal.tsx
+++ src/components/UI/BookModal.tsx
@@ -1,5 +1,35 @@
-// import React from "react";
+import { FC, ReactNode, useEffect, useMemo } from "react";
+import * as ReactDOM from "react-dom";
+import styles from "./ModalWrapper.module.scss";
 
-export default function BookModal() {
-  return <div></div>;
+interface ModalWrapperProps {
+  children: ReactNode;
+  toggleModal: any;
 }
+
+const ModalWrapper: FC<ModalWrapperProps> = ({ children, toggleModal }) => {
+  const containerElement: HTMLElement | null = useMemo(
+    () => document.getElementById("modal-container"),
+    []
+  );
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.removeProperty("overflow");
+    };
+  }, []);
+
+  if (!containerElement) {
+    return null;
+  }
+
+  return ReactDOM.createPortal(
+    <div className={styles.background} onClick={toggleModal}>
+      <div onClick={(e) => e.stopPropagation()}>{children}</div>
+    </div>,
+    containerElement
+  );
+};
+
+export default ModalWrapper;
